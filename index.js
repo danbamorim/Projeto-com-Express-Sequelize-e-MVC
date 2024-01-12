ab3bc65de9365f6e1396cfe5ba6dbf8ae8608681
Danilo Amorim <daniloamorim@MacBook-Pro-de-Danilo.local>
2024-01-12 16:42:48 -0300

    faltando alguns ajustes

--- index.js
+++ index.js
@@ -1,83 +1,77 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const app = express()
-const session = require('express-session')
-const conn = require('./db/conn')
-const FileStore = require('session-file-store')(session)
-const flash = require('express-flash')
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
 
-//template engine :
-// esse código configura o Express para usar o Handlebars como mecanismo de template para renderizar arquivos com extensão .handlebars, permitindo que o aplicativo renderize e exiba esses modelos usando o Handlebars como motor de renderização.
+const app = express();
+
+const conn = require("./db/conn");
+
+// Models
+const Tought = require("./models/Tought");
+
+// routes
+const toughtsRoutes = require("./routes/toughtsRoutes");
+const authRoutes = require("./routes/authRoutes");
+const ToughController = require("./controllers/ToughtController");
 
 app.engine('handlebars', exphbs.engine());
-
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 
 app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
 //session middleware
-// middleware de sessão permite que você armazene dados do usuário de maneira temporária ou persistente durante várias solicitações HTTP, o que é útil para autenticação, armazenamento de carrinho de compras, preferências do usuário, entre outros cenários.
-
-app.use(session({
-    name: "session",
-    secret: "nosso_secret",
+app.use(
+  session({
+    name: 'session',
+    secret: 'nosso_secret',
     resave: false,
     saveUninitialized: false,
     store: new FileStore({
-        logFn: function () {},
-        path: require('path').join(require('os').tmpdir(), 'session'),
+      logFn: function () {},
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
     }),
     cookie: {
-        secure: false,
-        maxAge: 3600000,
-        expires: new Date(Date.now() + 3600000),
-        httpOnly: true
-    }
-}))
+      secure: false,
+      maxAge: 3600000,
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    },
+  }),
+)
 
-//flash messages
-//Isso é útil para transmitir mensagens entre diferentes requisições e respostas, principalmente após redirecionamentos, fornecendo feedback ao usuário sobre ações realizadas no servidor.
-app.use(flash())
+// flash messages
+app.use(flash());
 
-// public path
-// É uma maneira conveniente de fornecer acesso a arquivos estáticos, como folhas de estilo, scripts JavaScript e imagens, sem a necessidade de configurar manualmente rotas para cada um deles.
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-//Tought
+// set session to res
+app.use((req, res, next) => {
+  // console.log(req.session)
+  console.log(req.session.userid);
 
-const ToughtController = require('./controllers/ToughtController')
-const toughtsRoutes = require('./routes/toughtsRoutes')
-app.get('/', ToughtController.showToughts)
-app.use('/toughts', toughtsRoutes)
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
 
+  next();
+});
 
-//set session to res
-// sempre que esse middleware for chamado (normalmente em todas as requisições), se houver um userid na sessão, as informações da sessão serão disponibilizadas para o template/renderização de forma local, acessíveis através de res.locals.session nas views do Express.
-app.use((req,res,next)=> {
-    if(req.session.userid){
-        res.locals.session = req.session
-    }
-    next()
-})
+app.use("/toughts", toughtsRoutes);
+app.use("/", authRoutes);
 
+app.get("/", ToughController.showToughts);
 
-//models
-const Tought = require('./models/Tought')
-const User = require('./models/User')
-
-
-//Import Rotas
-const authRoutes = require('./routes/authRoutes')
-
-// Rotas
-app.use('/' , authRoutes)
-
-conn.sync().then(() => { 
-    app.listen(3000)
-}).catch((error) => console.log(error))
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
